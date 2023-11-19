32e9eff15faf78468c47050ba5353dbb705bc335
Natália Alves Brito <Natalia_alves_b@hotmail.com>
2023-11-19 17:56:55 -0300

    variaveis para mandar pro firebase

--- Website (preview)/script.js
+++ Website (preview)/script.js
@@ -1,5 +1,6 @@
 console.log("O script está sendo executado");
 
+/*
 class Editor {
     constructor(elementId) {
         this.element = document.getElementById(elementId);
@@ -12,6 +13,29 @@ class Editor {
         }
     }
 }
+*/
+
+// teste editor novo
+class Editor {
+    constructor(elementId) {
+        this.element = document.getElementById(elementId);
+    }
+
+    editar() {
+        var novoValor = prompt(`Digite o novo valor para ${this.element.id}:`);
+        if (novoValor !== null) {
+            this.element.textContent = novoValor;
+            return novoValor; // Retorna o novo valor
+        }
+        return null; // Retorna null se o usuário cancelar
+    }
+
+    getValor() {
+        return this.element.textContent; // Retorna o valor atual do elemento
+    }
+}
+
+
 
 class Navegador {
     constructor(elementId, page) {
@@ -25,15 +49,37 @@ class Navegador {
 }
 
 // Instanciando objetos para edição
+const nomeEditor = new Editor("nome");
 const idadeEditor = new Editor("idade");
 const pesoEditor = new Editor("peso");
 const racaEditor = new Editor("raça");
 const corEditor = new Editor("cor");
 
 // Adicionando event listeners para edição
+
+/*
 document.getElementById("editarIdade").addEventListener("click", function() {
     idadeEditor.editar();
 });
+*/
+
+
+document.getElementById("editarIdade").addEventListener("click", function() {
+    const novoValorIdade = idadeEditor.editar(); // Chama o método editar() e armazena o novo valor retornado
+
+    // Verifica se o novo valor não é null (ou seja, se o usuário não cancelou a edição)
+    if (novoValorIdade !== null) {
+        console.log('Novo valor de idade:', novoValorIdade);
+        // Faça o que precisar com o novo valor de idade aqui
+    } else {
+        console.log('Edição cancelada ou sem alterações.');
+    }
+});
+
+
+
+
+
 
 document.getElementById("editarPeso").addEventListener("click", function() {
     pesoEditor.editar();
--- Website (preview)/scriptalimentacao.js
+++ Website (preview)/scriptalimentacao.js
@@ -46,4 +46,51 @@ document.addEventListener('DOMContentLoaded', function() {
         saudeNav.navegar();
     });
 
+
+    // alterando horários
+
+    // Selecionando os inputs de hora e minuto
+    const inputHora1 = document.querySelector('input[name="hora1"]');
+    const inputMinuto1 = document.querySelector('input[name="minuto1"]');
+
+    // Adicionando eventos de mudança (change) para os inputs
+    inputHora1.addEventListener('change', () => {
+        // Armazenando o novo valor da hora em uma variável
+        const novoValorHora1 = inputHora1.value;
+        console.log('Novo valor da hora:', novoValorHora1);
+        // Você pode fazer o que quiser com o novo valor da hora aqui
+    });
+
+    inputMinuto1.addEventListener('change', () => {
+        // Armazenando o novo valor do minuto em uma variável
+        const novoValorMinuto1 = inputMinuto1.value;
+        console.log('Novo valor do minuto:', novoValorMinuto1);
+        // Você pode fazer o que quiser com o novo valor do minuto aqui
+    });
+
+
+
+    
+    const inputHora2 = document.querySelector('input[name="hora2"]');
+    const inputMinuto2 = document.querySelector('input[name="minuto2"]');
+
+    // Adicionando eventos de mudança (change) para os inputs
+    inputHora1.addEventListener('change', () => {
+        // Armazenando o novo valor da hora em uma variável
+        const novoValorHora2 = inputHora2.value;
+        console.log('Novo valor da hora:', novoValorHora2);
+        // Você pode fazer o que quiser com o novo valor da hora aqui
+    });
+
+    inputMinuto1.addEventListener('change', () => {
+        // Armazenando o novo valor do minuto em uma variável
+        const novoValorMinuto2 = inputMinuto2.value;
+        console.log('Novo valor do minuto:', novoValorMinuto2);
+        // Você pode fazer o que quiser com o novo valor do minuto aqui
+    });
+
+
+
+
+
 });
